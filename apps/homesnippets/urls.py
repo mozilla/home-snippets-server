7ca1f1fc7f2ef03375df23c70f86ed30d22ca825
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-10-12 14:09:21 -0400

    Fix to url pattern for index; add index.wsgi

--- apps/homesnippets/urls.py
+++ apps/homesnippets/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns("homesnippets.views",
 
     # browser/components/nsBrowserContentHandler.js:911:    const SNIPPETS_URL = "http://snippets.mozilla.com/" + STARTPAGE_VERSION + "/%NAME%/%VERSION%/%APPBUILDID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/";
     url(r'^(?P<STARTPAGE_VERSION>[^/]+)/(?P<NAME>[^/]+)/(?P<VERSION>[^/]+)/(?P<APPBUILDID>[^/]+)/(?P<BUILD_TARGET>[^/]+)/(?P<LOCALE>[^/]+)/(?P<CHANNEL>[^/]+)/(?P<OS_VERSION>[^/]+)/(?P<DISTRIBUTION>[^/]+)/(?P<DISTRIBUTION_VERSION>[^/]+)/$', 
-        'view_snippet', name='view_snippets'),
+        'view_snippets', name='view_snippets'),
 
     url(r"^$", "index", name="index"),
 )
