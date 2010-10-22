22924e111f65c090452f70fdf8c7735aaba1dab1
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-10-22 15:52:44 -0400

    Get rid of hardcoded debug snippet, drop XHTML wrapper

--- apps/homesnippets/views.py
+++ apps/homesnippets/views.py
@@ -25,36 +25,32 @@ HTTP_MAX_AGE = getattr(settings, 'SNIPPET_HTTP_MAX_AGE', 1)
 DEBUG = getattr(settings, 'DEBUG', False)
 
 
-#@cache_page(HTTP_MAX_AGE, key_prefix='homesnippets')
-#@vary_on_headers('User-Agent', 'Accept-Language')
+#@cache_page(HTTP_MAX_AGE, key_prefix='homesnippets_view_snippets')
 @cache_control(public=True, max_age=HTTP_MAX_AGE)
 def view_snippets(request, **kwargs):
     """Fetch and render snippets matching URL segment args"""
-    out = [ '<div class="snippet">\n%s\n</div>' % snippet['body']
-        for snippet in Snippet.objects.find_snippets_with_match_rules(kwargs) ]
 
-    if DEBUG:
-        # HACK: This should probably be some sort of optional toolbar or
-        # bookmarklet given to QA / devs
-        from time import gmtime, strftime
-        time_now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-        out.append("""<div class="snippet">
-            <hr /> 
-            <p><a href="javascript:void(function%20()%20%7BlocalStorage%5B'snippets-last-update'%5D%20%3D%200%3B%20localStorage%5B'snippets'%5D%20%3D%20null%3B%20window.location.reload()%7D())">Click here to force refresh snippets</a></p>
-            <p>Snippets last generated at: """ + time_now + """</p>
-            <hr /> 
-        </div>""")
+    snippets = Snippet.objects.find_snippets_with_match_rules(kwargs)
+
+    out = [ snippet['body'] for snippet in snippets ]
 
     out.append('<!-- content generated at %s -->' %
-            strftime('%Y-%m-%dT%H:%M:%SZ', gmtime()))
-    resp = HttpResponse("""<?xml version="1.0" encoding="utf-8"?>
-        <div xmlns="http://www.w3.org/1999/xhtml" class="snippet_set">%s</div>
-    """ % "\n\n".join(out))
-    resp['Access-Control-Allow-Origin'] = '*'
+        strftime('%Y-%m-%dT%H:%M:%SZ', gmtime()))
+
+    out_txt = '<div class="snippet_set">%s</div>' % "\n\n".join(out)
+
+    resp = HttpResponse(out_txt)
+
+    # TODO: bug 606555 - Get ACAO working with about:home?
+    # resp['Access-Control-Allow-Origin'] = 'about:home' 
+    # resp['Access-Control-Allow-Origin'] = 'about:home, http%s://%s' % ( 
+    #     request.is_secure() and 's' or '', request.META['HTTP_HOST']) 
+    resp['Access-Control-Allow-Origin'] = '*' 
+    resp['Access-Control-Max-Age'] = HTTP_MAX_AGE
+    resp['Access-Control-Allow-Methods'] = 'GET, HEAD, OPTIONS'
+
     return resp
 
-#@cache_page(HTTP_MAX_AGE, key_prefix='homesnippets')
-#@vary_on_headers('User-Agent', 'Accept-Language')
 @cache_control(public=True, max_age=HTTP_MAX_AGE)
 def index(request):
     """Render the index page, simulating about:home"""
