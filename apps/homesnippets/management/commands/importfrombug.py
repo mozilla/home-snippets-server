f0e51bf4603bf0885a893523929fe4ed8c7b00e6
Leslie Michael Orchard <l.m.orchard@pobox.com>
2011-03-10 13:43:39 -0500

    Reworked import command to start using external JSON build file

--- apps/homesnippets/management/commands/importfrombug.py
+++ apps/homesnippets/management/commands/importfrombug.py
@@ -14,60 +14,20 @@ run where they're missing. Subsequent runs should update existing snippets and
 rules with new content, leaving primary keys alone. That should ease the bulk
 export and load of model objects as JSON from the Django admin pages.
 """
-import os, sys, re, urllib, json, base64
+import os, sys, re, cgi, urllib, json, base64
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from homesnippets.models import ClientMatchRule, Snippet
 
 
-# TODO: Most/all of these constants should be specified via a JSON build
-# config file and/or command-line options to make this command more flexible.
-
-BUG_ID = '636815'
-
 # URL for JSON representation of known shipping languages
 LANG_JSON_URL = getattr(settings, 'LANG_JSON_URL', 
     'http://svn.mozilla.org/libs/product-details/json/languages.json')
+
 # URL template for grabbing a bug's attachments in JSON form.
 BUG_JSON_URL = getattr(settings, 'BUG_JSON_URL',
     'https://api-dev.bugzilla.mozilla.org/latest/bug/%(bug_id)s/attachment?attachmentdata=1')
-
-# Description template used for naming per-locale client match rules
-RULE_DESCRIPTION_TMPL = '%(code)s locale (%(name)s), Firefox v4.*'
-# Name template used in naming each generated snippet
-SNIPPET_NAME_TMPL = 'Bug %(bug_id)s: %(attachment_description)s - %(english)s'
-# HTML template used to construct HTML content for each snippet.
-SNIPPET_HTML_TMPL = """<div class="snippet">
-    <img src="%(icon_url)s" />
-    <p>%(translation)s</p>
-</div>"""
-
-# Metadata associated with each snippet by English text
-# icon_url: a data: URI for an icon image
-# snippet_url: URL used for the single %s placeholder 
-TRANSLATIONS_META = {
-    """Firefox 4 is here: new look, more speed, extra awesomeness. <a href="%s">Download today!</a>""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Firefox 4 is here! <a href="%s">Download it today</a> and put the Web on fast forward.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Take Firefox 4 anywhere you go! Firefox 4 for Android and Maemo is live and ready for you to enjoy. <a href="%s">Get it now</a>.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Now that you've got Firefox 4, <a href="%s">learn about all the cool things</a> you can do with your new browser.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Want to see what the Web is really capable of? <a href="%s">Explore the Web o' Wonder demo gallery</a> and prepare to be amazed!""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Help us share Firefox 4 with the world. <a href="%s">Be a part of Team Firefox!</a>""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Everyone's Web is a bit different... What does yours look like? Visualize your Internet at <a href="%s">Webify Me</a>.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Supercharge your Firefox with our <a href="%s">newest collection of add-ons!</a>""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """The Web belongs to all of us. <a href="%s">Make your mark</a> and show your support for the world's largest public resource.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-    """Celebrate Firefox going mobile with Spark, a free game for Android devices. <a href="%s">Get started</a>.""": 
-        dict( icon_url='', snippet_url='http://example.com' ),
-}
 
 
 class Command(BaseCommand):
@@ -77,34 +37,46 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        if len(args) < 1:
+            print "Usage: manage.py importfrombug import-manifest.json"
+            sys.exit(1)
+
+        self.import_manifest = json.load(open(args[0], 'r'))
+
         # TODO: Read all of this in from a JSON build file.
-        self.bug_id = BUG_ID
-        self.rule_description_tmpl = RULE_DESCRIPTION_TMPL
-        self.snippet_name_tmpl = SNIPPET_NAME_TMPL
-        self.snippet_html_tmpl = SNIPPET_HTML_TMPL
-        self.translations_meta = TRANSLATIONS_META
+        self.bug_id = self.import_manifest['bug_id']
 
         self.update_locale_rules()
         self.update_snippets()
 
     def update_locale_rules(self):
         """Ensure a ClientMatchRule exists for each known locale"""
+
         print "Fetching Firefox product languages"
         self.lang_data = json.load(urllib.urlopen(LANG_JSON_URL))
+
+        # Built a case-insensitive map of known locale codes.
+        self.locales = dict()
+        for code, details in self.lang_data.items():
+            self.locales[code.lower()] = dict( code=code, name=details['English'] )
 
         print "Updating locale match rules"
         ( rules_created, rules_updated ) = ( [], [] )
         for code, details in self.lang_data.items():
 
+            defaults = dict()
+            for k, v in self.import_manifest['rule_defaults'].items():
+                defaults[k] = v % dict(
+                    locale=code, 
+                    name=details['English']
+                )
+
             ( rule, created ) = ClientMatchRule.objects.get_or_create(
-                description=self.rule_description_tmpl % dict(
-                    name=details['English'], code=code))
+                description=defaults['description'])
             
-            # TODO: Read all of this in from a JSON build file.
-            rule.version='/4\.0.*/'
-            rule.locale=code
-            rule.startpage_version = '1'
-            rule.name = 'Firefox'
+            for k, v in defaults.items():
+                setattr(rule, k, v)
+
             rule.save()
 
             if created:
@@ -114,17 +86,14 @@ class Command(BaseCommand):
 
         print "Created: %s" % rules_created
         print "Updated: %s" % rules_updated
-
-        # Built a case-insensitive map of known locale codes.
-        self.locales = dict()
-        for code, details in self.lang_data.items():
-            self.locales[code.lower()] = dict( code=code, name=details['English'] )
         
     def update_snippets(self):
         """Update snippets from bug attachments"""
 
         print "Fetching data for bug %s" % self.bug_id
         self.bug_data = json.load(urllib.urlopen(BUG_JSON_URL % {'bug_id':self.bug_id}))
+
+        icon_img_cache = dict()
 
         # Iterate through all attachments found in the bug. Parse out locale
         # code from description, decode data and parse out english/translation
@@ -148,17 +117,18 @@ class Command(BaseCommand):
                 print 'BAD LOCALE: %s' % (matches[-1])
                 continue
 
-            print "\n%s\n" % ( locale, )
+            print "%s" % ( locale, )
 
             # Look up the match rule created earlier for this locale.
             rule = ClientMatchRule.objects.get(
-                description=self.rule_description_tmpl % dict(
-                    name=locale['name'], code=locale['code']))
+                description=self.import_manifest['rule_defaults']['description'] % dict(
+                    name=locale['name'], locale=locale['code']))
 
             # Attachment data should show up in the JSON as base64. Haven't
             # seen another kind of encoding so far, but we could be surprised.
             if 'base64' == attachment['encoding']:
-                data = base64.b64decode(attachment['data'])
+                data = unicode(base64.b64decode(attachment['data']), 'utf-8')
+                open('trans-%s.txt' % locale['code'], 'w').write(data.encode('utf-8'))
             else:
                 bad_attachments.append(attachment)
                 print 'BAD DATA: %s' % (matches[-1])
@@ -171,10 +141,12 @@ class Command(BaseCommand):
             # 
             # So, parse that into english/translated pairs.
             snippets, english, translated = [], None, None
-            for line in data.splitlines():
+            lines = data.splitlines() + [u'']
+            BOM_SEMI = unicode("\xef\xbb\xbf;", 'utf-8')
+            for line in lines:
                 line = line.strip()
-                # HACK: Some attachments started with UTF-8 marker, so look for that too
-                if line.startswith(";") or line.startswith("\xef\xbb\xbf;"):
+                # HACK: Some attachments started with UTF-8 BOM marker, so look for that too
+                if line.startswith(";") or line.startswith(BOM_SEMI):
                     english = line[line.index(';')+1:]
                 elif line:
                     translated = line
@@ -187,25 +159,56 @@ class Command(BaseCommand):
             # through what we have, pair each up with snippet metadata,
             # create/update a snippet from templates filled with translation
             # and metadata
+            s_updated, s_created, idx = [], [], 0
             for english, translated in snippets:
-                meta = self.translations_meta[english]
-                snippet_html = self.snippet_html_tmpl % dict(
-                    icon_url=meta['icon_url'],
-                    translation=translated % ( meta['snippet_url'], )
-                )
+                idx += 1
 
-                ( snippet, created ) = Snippet.objects.get_or_create(
-                    name=self.snippet_name_tmpl % dict( 
-                        bug_id = self.bug_id, 
-                        attachment_description = attachment['description'],
-                        english = english
-                    ))
-                snippet.body = snippet_html
+                if english not in self.import_manifest['snippets_content']:
+                    print "\t%s BAD ENGLISH: %s" % (idx, english)
+                    continue
+
+                meta = self.import_manifest['snippets_content'][english]
+                meta.update(dict(
+                    bug_id = self.bug_id,
+                    attachment_description = attachment['description'],
+                    english = english,
+                    translated = translated % urllib.quote(meta['placeholder']),
+                ))
+
+                # Quick and dirty image-to-data: URL conversion if there's an
+                # icon_url_src pointing at an image.
+                if 'icon_url_src' in meta and meta['icon_url_src']:
+                    if meta['icon_url_src'] not in icon_img_cache:
+                        data = urllib.urlopen(meta['icon_url_src'])
+                        enc = urllib.quote(base64.b64encode(data.read()))
+                        type = data.info()['content-type'].split(';')[0]
+                        icon_img_cache[meta['icon_url_src']] = ('data:%s;base64,%s' % ( type, enc ))
+                    meta['icon_url'] = icon_img_cache[meta['icon_url_src']] 
+                else :
+                    meta['icon_url'] = ''
+
+                snippet_props = dict()
+                for k,v in self.import_manifest['snippet_defaults'].items():
+                    snippet_props[k] = v % meta
+                snippet_props.update(meta)
+
+                ( snippet, created ) = Snippet.objects.get_or_create(name=snippet_props['name'])
+                for k,v in snippet_props.items():
+                    setattr(snippet, k, v);
                 snippet.client_match_rules = [ rule ]
                 snippet.save()
 
-                print "\t(%s) %s" % ( ['updated','created'][created], english )
+                # TODO: Need a -verbose level option to turn this on and off
+                # print "\t%s (%s) %s" % ( idx, ['updated','created'][created], english )
+
+                if created: s_created.append(snippet)
+                else: s_updated.append(snippet)
+
+            # TODO: Need a -verbose level option to turn this on and off
+            print "Created %s / Updated %s" % ( len(s_created), len(s_updated) )
 
         # Report on bad attachments whose locale or data was unreadable.
-        # TODO: Look out for errors in the per-snippet semicolon encoding
+        # TODO: Need a -verbose level option to turn this on and off
         print "BAD: %s" % ( len(bad_attachments) )
+        for attachment in bad_attachments:
+            print attachment
