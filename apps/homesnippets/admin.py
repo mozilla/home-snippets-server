379857d79d6e95fa4c0ee923e6b96c6feb52f7f0
Leslie Michael Orchard <l.m.orchard@pobox.com>
2011-04-25 11:52:17 -0400

    Add admin action to dump just selected objects (ie. snippets and match rules)

--- apps/homesnippets/admin.py
+++ apps/homesnippets/admin.py
@@ -1,11 +1,38 @@
+import os
+from datetime import datetime
+
 from django.contrib import admin
 from django import forms
 from django.db import models
 
+from django.db.models import get_app, get_apps, get_model, get_models
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from smuggler.settings import SMUGGLER_FORMAT, SMUGGLER_FIXTURE_DIR
+from smuggler.utils import (get_excluded_models_set, get_file_list,
+                            save_uploaded_file_on_disk, serialize_to_response,
+                            superuser_required)
+
 from homesnippets.models import Snippet, ClientMatchRule
+
+
+def dump_selected(modeladmin, request, queryset):
+    """Produce a smuggler dump for a selected set of model objects"""
+    objects = queryset.all()
+    filename = '%s-%s_%s.%s' % ('homesnippets', modeladmin.dump_name,
+                                datetime.now().isoformat(), SMUGGLER_FORMAT)
+    response = HttpResponse(mimetype="text/plain")
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return serialize_to_response(objects, response)
+
+dump_selected.short_description = "Dump selected objects as JSON data"
+
 
 class ClientMatchRuleAdmin(admin.ModelAdmin):
     change_list_template = 'smuggler/change_list.html'
+
+    actions = [ dump_selected ]
+    dump_name = 'clientmatchrules'
 
     list_per_page = 250
 
@@ -33,6 +60,9 @@ admin.site.register(ClientMatchRule, ClientMatchRuleAdmin)
 
 class SnippetAdmin(admin.ModelAdmin):
     change_list_template = 'smuggler/change_list.html'
+
+    actions = [ dump_selected ]
+    dump_name = 'snippets'
     
     save_on_top = True
     actions_on_bottom = True
