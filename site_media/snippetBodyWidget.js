62ca26b17bfb4dd5c07f38519866686f044ce3e0
Michael Kelly <mkelly@mozilla.com>
2011-06-14 16:58:30 -0700

    Improve UI, remove auto-icon-embed, and add wiki-links
     - UI rearranged for bigger input boxes
     - User now has to click a button to embed an icon, making it harder
       to lose an icon while making changes
     - Links can be added using a wiki-like syntax:
       [http://link.url|Link text]

--- site_media/snippetBodyWidget.js
+++ site_media/snippetBodyWidget.js
@@ -1,46 +1,73 @@
 (function($) {
 $(function() {
-	var icon_url = $("#snippet-body-icon-url");
-	var snippet_text = $("#snippet-body-text");
-	var preview = $("#snippet-body-preview");
+	var icon_url = $("#snippet-icon-url");
+	var snippet_text = $("#snippet-text");
+	var preview = $("#snippet-preview");
 	var form_textarea = $("#id_body");
+
+	var preview_icon_url = "";
+	var preview_icon_data = "";
+
+	function wiki2link(str) {
+		return str.replace(/\[(.+)\|(.+)\]/, '<a href="$1">$2</a>');
+	}
+
+	function link2wiki(str) {
+		return str.replace(/<a href="(.+)">(.+)<\/a>/, "[$1|$2]");
+	}
 	
-	function generateSnippet(iconURI) {
-		var snippet = '<div class="snippet">';
-		if (iconURI != '') {
-			snippet += '<img class="icon" src="' + iconURI + '" />';
+	// Generate snippet code and fill form/preview with it
+	function generateSnippet() {
+		// Add comment to easily identify "basic" snippets
+		var snippet = '<!--basic--><div class="snippet">';
+		if (preview_icon_data != '') {
+			snippet += '<img class="icon" src="' + preview_icon_data + '" />';
 		}
-		snippet += '<p>' + snippet_text.val() + '</p></div>';
+		snippet += '<p>' + wiki2link(snippet_text.val()) + '</p></div>';
 
 		form_textarea.val(snippet);
 		preview.html(snippet);
 	}
 
-	var preview_icon_url = "";
-	var preview_icon_data = "";
-	function processBasicForm() {
+	function encodeIcon() {
 		// Avoid encoding when we don't need to
 		if (preview_icon_url != icon_url.val()) {
-			preview_icon_url = icon_url.val();
-
 			// TODO: Support non-png images
+			var image_to_encode = icon_url.val();
 			$.ajax({
-				url: '/base64encode/' + preview_icon_url,
+				url: '/base64encode/' + image_to_encode,
 				dataType: 'json',
 				error: function() {
-					generateSnippet("");
+					alert("Error encoding icon. Please check that the icon URL points to a valid PNG image.");
 				},
 				success: function(data) {
+					preview_icon_url = image_to_encode;
 					preview_icon_data = 'data:image/png;base64,' + data['img'];
-					generateSnippet(preview_icon_data);
+					generateSnippet();
 				}
 			});
-		} else {
-			generateSnippet(preview_icon_data);
 		}
 	}
 
-	$('#snippet-body-text, #snippet-body-icon-url').bind('change keyup', processBasicForm);
-	$('#snippet-body-editor').easytabs();
+	// Parse snippet code and fill in basic form with pulled info
+	(function() {
+		var snippet_code = form_textarea.val();
+		if (snippet_code.match(/<!--basic-->/) === null) return;
+		
+		// Be dumb and throw a regex at it
+		var img_matches = snippet_code.match(/<img class="icon" src="([\s\S]+)" \/>/);
+		if (img_matches === null) return;
+		preview_icon_data = img_matches[1];
+
+		var text_matches = snippet_code.match(/<p>(.+)<\/p>/);
+		if (text_matches === null) return;
+
+		snippet_text.val(link2wiki(text_matches[1]));
+		generateSnippet();
+	})();
+
+	$('#snippet-text').bind('change keyup', generateSnippet);
+	$('#snippet-embed-button').click(encodeIcon);
+	$('#snippet-editor').easytabs();
 });
 })(jQuery);
