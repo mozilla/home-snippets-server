c2c8cb417374724fc9b8b0f9266af96bdb996788
Michael Kelly <mkelly@mozilla.com>
2011-06-17 14:19:28 -0700

    Document and clean up forms.py

--- apps/homesnippets/forms.py
+++ apps/homesnippets/forms.py
@@ -1,11 +1,18 @@
-from django.forms import *
+"""
+Form classes
+"""
+
+from django.forms import Form, DateTimeField, DateInput, CharField, widgets
+
 
 class BulkDateForm(Form):
+    """Form for changing the start and end dates of many snippets at once"""
+
     start_date = DateTimeField(label='Start Date',
-                               widget=DateInput(attrs={'class':'datetime'}),
+                               widget=DateInput(attrs={'class': 'datetime'}),
                                required=False)
     end_date = DateTimeField(label='End Date',
-                               widget=DateInput(attrs={'class':'datetime'}),
+                             widget=DateInput(attrs={'class': 'datetime'}),
                              required=False)
     ids = CharField(required=True,
                     widget=widgets.HiddenInput())
