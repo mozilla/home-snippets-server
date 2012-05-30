87d8fc69c2d93af1c7c9a09ae0947ed7c17d2e52
Michael Kelly <mkelly@mozilla.com>
2012-05-30 16:48:20 -0400

    Add tests for getattribute template tag.

--- apps/homesnippets/tests/test_templatetags.py
+++ apps/homesnippets/tests/test_templatetags.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+from django.conf import settings
 from django.template import Context
 from django.template.loader import get_template_from_string
 from django.test import TestCase
@@ -25,3 +28,40 @@ class TestICanHaz(TestCase):
         eq_(result, 'Nyan [Nyan] {{Nyan}} {{{Nyan}}}',
             "Single brackets should' be preserved, but double and triple "
             "brackets should be replaced.")
+
+
+class TestGetAttribute(TestCase):
+    def setUp(self):
+        """Create empty context to render test templates with."""
+        self.context = Context()
+
+    def test_basic(self):
+        """Test if getattribute can retrieve an object attribute."""
+        Point = namedtuple('Point', ['x', 'y'])
+        self.context['p'] = Point(4, 5)
+
+        template_str = """
+        {% load getattribute %}
+        {{ p|getattribute:'x' }}
+        """
+        template = get_template_from_string(template_str)
+        result = template.render(self.context).strip()
+
+        eq_(result, '4')
+
+    def test_nonexistant_attribute_returns_invalid(self):
+        """
+        If the requested attribute doesn't exist, return the invalid template
+        string.
+        """
+        Point = namedtuple('Point', ['x', 'y'])
+        self.context['p'] = Point(4, 5)
+
+        template_str = """
+        {% load getattribute %}
+        {{ p|getattribute:'q' }}
+        """
+        template = get_template_from_string(template_str)
+        result = template.render(self.context).strip()
+
+        eq_(result, settings.TEMPLATE_STRING_IF_INVALID)
